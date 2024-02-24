a2fcbbd18037fba1fb9224d0acc2e63b3d821286
vlad <mainvgd@gmail.com>
2024-02-24 20:04:32 +0300

    optimize imports

--- src/main/java/by/vlad/gusakov/taskmanagementsystem/service/UserService.java
+++ src/main/java/by/vlad/gusakov/taskmanagementsystem/service/UserService.java
@@ -6,7 +6,6 @@ import by.vlad.gusakov.taskmanagementsystem.model.User;
 import by.vlad.gusakov.taskmanagementsystem.repository.UserRepository;
 import by.vlad.gusakov.taskmanagementsystem.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
